2a7a7e8b1731c7ee437cf8845bd5465740b6c9b1
rans0000 <rans0000@gmail.com>
2025-05-25 00:58:03 +0530

    add a pause menu to the game

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,11 +1,13 @@
 extends Control
 
 @export_file("*.tscn") var new_game_path: String
+@export_file("*.tscn") var pause_menu_path: String
 
 
 func _on_play_button_pressed() -> void:
 	Global.game_controller.unload_all_scenes()
 	Global.game_controller.change_3D_scene(new_game_path)
+	Global.game_controller.change_gui_scene(pause_menu_path)
 
 
 func _on_quit_button_pressed() -> void:
---
+++ scripts/pause_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@export_file("*tscn") var main_menu_path : String
+
+
+
+func _enter_tree() -> void:
+	hide()
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		var tree := get_tree()
+		if tree.paused: resume_game(tree)
+		else: pause_game(tree)
+
+
+func resume_game(tree: SceneTree) -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	tree.paused = false
+	self.hide()
+
+
+func pause_game(tree: SceneTree) -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	tree.paused = true
+	self.show()
+
+
+func _on_resume_btn_pressed() -> void:
+	resume_game(get_tree())
+
+
+func _on_main_menu_btn_pressed() -> void:
+	get_tree().paused = false
+	Global.game_controller.unload_all_scenes()
+	Global.game_controller.change_gui_scene(main_menu_path)
+
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
