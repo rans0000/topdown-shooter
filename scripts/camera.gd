09565d5ab43e86fbb2d3838b7744d24cf6be782d
rans0000 <rans0000@gmail.com>
2025-06-01 14:47:24 +0530

    fix player rotation bug when animating

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -6,5 +6,6 @@ var mouse_acceleration := 7
 
 
 
-func _process(delta: float) -> void:
-	position = lerp(position, camera_target.global_position, mouse_acceleration * delta)
+func _process(_delta: float) -> void:
+	#position = lerp(position, camera_target.global_position, mouse_acceleration * delta)
+	position = camera_target.global_position
